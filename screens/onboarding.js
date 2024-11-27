bbba14dbb3566254a6f047e1452b12b039f7bca4
Hasansarwer <hasanmathju@gmail.com>
2024-11-28 00:42:27 +0600

    images added and fixed first page of onboarding

--- screens/onboarding.js
+++ screens/onboarding.js
@@ -8,12 +8,14 @@ import {
     Image,
     StyleSheet
  } from "react-native";
+import PagerView from "react-native-pager-view";
 import { ProgressBar, MD3Colors } from "react-native-paper";
 // import PagerView from 'react-native-pager-view';
 
 const Onboarding = ({navigation, completeOnboarding}) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [currentPage, setCurrentPage] = useState(0);
     const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
     const handaleNext = async () => {
@@ -32,49 +34,47 @@ const Onboarding = ({navigation, completeOnboarding}) => {
     }, [name, email]);
 
     return (
-        // <PagerView style={{ flex: 1 }} initialPage={0} onPageSelected={(e) => setCurrentPage(e.nativeEvent.position)}>
-            <View key="1" style={{ flex: 1,  alignItems: "center"}}>
+        <PagerView style={{ flex: 1 }} initialPage={0} onPageSelected={(e) => setCurrentPage(e.nativeEvent.position)}>
+            <View key="1" style={{ flex: 1,  alignItems: "center", alignContent: "space-between"}}>
                 <View style={{height: 100, width: "100%", flexDirection: "row", justifyContent: "center", alignItems: "center", backgroundColor: "#dee3e9" }}>
-                    <Image source={require("../assets/little-lemon-logo-grey.png")} style={{ width: 50, height: 60 }} />
-                    <Text style={{ fontSize: 24, fontWeight: "bold", padding:10 }}>LITTLE LEMON</Text>
+                    <Image source={require("../assets/Logo.png")} style={{ width: 185, height: 40 }} />
                 </View>
                 <View style={{flex: 1, alignItems: "center", width: "100%", backgroundColor: "#cbd2d9" }}>
                 {/* <Image source={require("../assets/onboarding-1.png")} style={{ width: 200, height: 200 }} /> */}
                     <Text style={{ fontSize: 20, fontWeight: "bold", textAlign: "center", paddingTop: 30 }}>Let us get to know you</Text>
                     <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: "center", marginTop: 100 }}>First Name</Text>
-                <TextInput
-                    // placeholder="Name"
-                    value={name}
-                    onChangeText={(text) => setName(text)}
-                    style={{ width: "80%", marginVertical: 10, padding: 10, borderColor: MD3Colors.grey500, borderWidth: 1, borderRadius: 5 }}
-                />
-                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: "center" }}>Email</Text>
-                <TextInput
-                    // placeholder="Email"
-                    value={email}
-                    onChangeText={(text) => setEmail(text)}
-                    style={{ width: "80%", marginVertical: 10, padding: 10, borderColor: MD3Colors.grey500, borderWidth: 1, borderRadius: 5 }}
-                />
-                {/* <Button style={{backgroundColor: '#fff', width: '25%',borderRadius:5}} title="Next" onPress={() => setCurrentPage(1)} /> */}
-                <View style={{ width: "100%", flexDirection: "row", justifyContent: "flex-end", marginTop: 50, backgroundColor: "#f0f0f0" }}>
+                    <TextInput
+                        // placeholder="Name"
+                        value={name}
+                        onChangeText={(text) => setName(text)}
+                        style={{ width: "80%", marginVertical: 10, padding: 10, borderColor: MD3Colors.grey500, borderWidth: 1, borderRadius: 5 }}
+                    />
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: "center" }}>Email</Text>
+                    <TextInput
+                        // placeholder="Email"
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                        style={{ width: "80%", marginVertical: 10, padding: 10, borderColor: MD3Colors.grey500, borderWidth: 1, borderRadius: 5 }}
+                    />
+                    {/* <Button style={{backgroundColor: '#fff', width: '25%',borderRadius:5}} title="Next" onPress={() => setCurrentPage(1)} /> */}
+                    
+                </View>
+                <View style={{ width: "100%", flexDirection: "row", justifyContent: "flex-end", backgroundColor: "#f0f0f0" }}>
                     <TouchableOpacity 
-                    // style={{ backgroundColor: "#cbd2d9", margin:30, paddingHorizontal: 20, paddingVertical:10, borderRadius: 5 }} 
-                    style={[
+                        // style={{ backgroundColor: "#cbd2d9", margin:30, paddingHorizontal: 20, paddingVertical:10, borderRadius: 5 }} 
+                        style={[
                         styles.button,
                         isButtonDisabled && styles.buttonDisabled, // Apply disabled styling
-                    ]}
-                    disabled={isButtonDisabled} 
-                    onPress={() => handaleNext()}
-                    >
+                        ]}
+                        disabled={isButtonDisabled} 
+                        onPress={() => handaleNext()}
+                        >
                         <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: "center" }} >Next</Text>
                     </TouchableOpacity>
                 </View>
-                </View>
                 
-            </View>
-            
-            
-        // </PagerView>
+            </View>  
+        </PagerView>
     );
 }
 
